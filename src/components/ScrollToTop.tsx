81cbfe395d04627ad17e56624b4b7046482bd040
Juan Abril <juanabca@unisabana.edu.co>
2024-10-18 18:52:40 -0500

    Add metrics component

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+function ScrollToTop() {
+  
+
+    useEffect(() => {
+        setTimeout(() => {
+          window.scrollTo(0, 0);
+        }, 100); 
+      }, []); 
+
+  return null;
+}
+
+export default ScrollToTop;
